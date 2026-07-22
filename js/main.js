5c50f9abde48141a31aa8480c8feb2d96b04cb30
darkprince7034-lgtm <darkprince7034@gmail.com>
2026-07-22 19:24:01 +0530

    Added confirmation window for subcribe

--- js/main.js
+++ js/main.js
@@ -93,3 +93,45 @@ if(progressBar){
     });
 
 }
+
+const subscribeBtn = document.getElementById("subscribeBtn");
+const telegramModal = document.getElementById("telegramModal");
+const continuePayment = document.getElementById("continuePayment");
+const closeModal = document.getElementById("closeModal");
+
+const paymentPage =
+"https://superprofile.bio/vig/6a5fc72a0f3a3c0014fc0912";
+
+if (subscribeBtn) {
+
+    subscribeBtn.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        telegramModal.classList.add("show");
+
+    });
+
+}
+
+continuePayment.addEventListener("click", function(){
+
+    window.location.href = paymentPage;
+
+});
+
+closeModal.addEventListener("click", function(){
+
+    telegramModal.classList.remove("show");
+
+});
+
+telegramModal.addEventListener("click", function(e){
+
+    if(e.target === telegramModal){
+
+        telegramModal.classList.remove("show");
+
+    }
+
+});
